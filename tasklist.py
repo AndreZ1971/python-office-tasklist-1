47342eb62e07b7d7acffa21dbd3d8aa133c467a7
AndreZ1971 <andre.zabel71@gmail.com>
2024-10-04 15:42:12 +0200

    Erstelle deine Aufgabe

---
+++ tasklist.py
@@ -0,0 +1,19 @@
+# Aufgabenliste initialisieren
+tasklist = []
+
+# Funktion zum Hinzufügen einer Aufgabe
+def add_task():
+    task = input("Bitte gib eine Aufgabe ein, die in deiner Aufgabenliste hinzugefügt werden soll: ")
+    due_date = input("Bitte gib ein Fälligkeitsdatum ein (TT.MM.JJJJ) oder drücke Enter, um keins anzugeben: ")
+    priority = input("Bitte gib die Priorität an (hoch, mittel, niedrig) oder drücke Enter für 'mittel': ")
+    
+    if not priority:
+        priority = "mittel"
+    
+    task_entry = {
+        "Aufgabe": task,
+        "Fälligkeitsdatum": due_date,
+        "Priorität": priority
+    }
+    tasklist.append(task_entry)
+    print(f"Aufgabe '{task}' wurde der Liste hinzugefügt.")
