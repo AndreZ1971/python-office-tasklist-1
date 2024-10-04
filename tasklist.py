634c29679606339568b836530b767c6223a950f6
AndreZ1971 <andre.zabel71@gmail.com>
2024-10-04 15:48:20 +0200

    Anzeigen der Aufgaben

--- tasklist.py
+++ tasklist.py
@@ -17,3 +17,26 @@ def add_task():
     }
     tasklist.append(task_entry)
     print(f"Aufgabe '{task}' wurde der Liste hinzugefügt.")
+
+
+from datetime import datetime, timedelta
+
+
+def show_tasklist():
+    if not tasklist:
+        print("Deine Aufgabenliste ist leer.")
+    else:
+        print("Deine Aufgabenliste:")
+        today = datetime.now().date()
+        for task in tasklist:
+            task_str = f"- {task['Aufgabe']}"
+            if task['Fälligkeitsdatum']:
+                due_date = datetime.strptime(task['Fälligkeitsdatum'], "%d.%m.%Y").date()
+                days_left = (due_date - today).days
+                task_str += f" (Fällig am {task['Fälligkeitsdatum']})"
+                # Markierung für Aufgaben die weniger als 48h Vorlauf haben
+                if days_left <= 2:
+                    task_str = f"\033[91m{task_str}\033[0m"  # wird Rot
+            if task['Priorität']:
+                task_str += f" [Priorität: {task['Priorität']}]"
+            print(task_str)
